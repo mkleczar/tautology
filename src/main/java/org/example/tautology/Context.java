deea0b7c8dd93b706aca68fab27b247f8779c314
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-04-04 20:56:35 +0200

    add context builder to store logical parameters validation

--- src/main/java/org/example/tautology/Context.java
+++ src/main/java/org/example/tautology/Context.java
@@ -1,27 +1,47 @@
 package org.example.tautology;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Context {
 
-    private final Set<String> params = new HashSet<>();
+    private final Map<String, Boolean> params;
 
-    // TODO: move this to builder
-    public Context setParam(String name, Boolean value) {
-        if (value) {
-            params.add(name);
-        }
-        return this;
+    private Context(Map<String, Boolean> params) {
+        this.params = params;
     }
 
-    // TODO: move this to builder
-    public Context paramTrue(String name) {
-        params.add(name);
-        return this;
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static Context empty() {
+        return new Context(new HashMap<>());
     }
 
     public Boolean check(String name) {
-        return params.contains(name);
+        return params.getOrDefault(name, false);
+    }
+
+    public static class Builder {
+        private Map<String, Boolean> params = new HashMap<>();
+
+        public Context build() {
+            var tmp = params;
+            params = null;
+            return new Context(tmp);
+        }
+
+        public Builder param(String name, Boolean value) {
+            params.put(name, value);
+            return this;
+        }
+
+        public Builder paramTrue(String name) {
+            return param(name, true);
+        }
+        public Builder paramFalse(String name) {
+            return param(name, false);
+        }
     }
 }
--- src/test/java/org/example/tautology/ExpressionTest.java
+++ src/test/java/org/example/tautology/ExpressionTest.java
@@ -5,40 +5,48 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.stream.Stream;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.example.tautology.Expression.*;
+import static org.example.tautology.Expression.variable;
+import static org.example.tautology.Expression.and;
+import static org.example.tautology.Expression.or;
+import static org.example.tautology.Expression.not;
+import static org.example.tautology.Expression.implication;
 
 @Slf4j
 public class ExpressionTest {
 
     @Test
     public void checkValueOfVariable() {
-        Context context = new Context()
-                .paramTrue("p");
-        Expression expression = variable("p");
-        assertThat(expression.validate(context)).isTrue();
-        assertThat(expression.validate(new Context())).isFalse();
+        Context context =  Context.builder()
+                .paramTrue("p")
+                .paramFalse("q")
+                .build();
+        Expression expressionTrue = variable("p");
+        assertThat(expressionTrue.validate(context)).isTrue();
+
+        Expression expressionFalse = variable("q");
+        assertThat(expressionFalse.validate(context)).isFalse();
+
+        assertThat(variable("x").validate(Context.empty())).isFalse();
     }
 
     @Test
     public void checkValueOfVariableNegation() {
-        Context context = new Context()
-                .paramTrue("p");
+        Context context = Context.builder()
+                .paramTrue("p").build();
         Expression expression = not(variable("p"));
         assertThat(expression.validate(context)).isFalse();
-        assertThat(expression.validate(new Context())).isTrue();
+        assertThat(expression.validate(Context.empty())).isTrue();
     }
 
     @ParameterizedTest
     @MethodSource("checkAlternativeOfVariableParams")
     public void checkAlternativeOfVariable(Boolean p, Boolean q, Boolean expected) {
-        Context context = new Context()
-                .setParam("p", p)
-                .setParam("q", q)
-                ;
+        Context context = Context.builder()
+                .param("p", p)
+                .param("q", q)
+                .build();
         Expression expression = or(variable("p"), variable("q"));
         assertThat(expression.validate(context)).isEqualTo(expected);
     }
@@ -55,10 +63,10 @@ public class ExpressionTest {
     @ParameterizedTest
     @MethodSource("checkConjunctionOfVariableParams")
     public void checkConjunctionOfVariable(Boolean p, Boolean q, Boolean expected) {
-        Context context = new Context()
-                .setParam("p", p)
-                .setParam("q", q)
-                ;
+        Context context = Context.builder()
+                .param("p", p)
+                .param("q", q)
+                .build();
         Expression expression = and(variable("p"), variable("q"));
         assertThat(expression.validate(context)).isEqualTo(expected);
     }
@@ -75,10 +83,10 @@ public class ExpressionTest {
     @ParameterizedTest
     @MethodSource("checkImplicationOfVariableParams")
     public void checkImplicationOfVariable(Boolean p, Boolean q, Boolean expected) {
-        Context context = new Context()
-                .setParam("p", p)
-                .setParam("q", q)
-                ;
+        Context context = Context.builder()
+                .param("p", p)
+                .param("q", q)
+                .build();
         Expression expression = implication(variable("p"), variable("q"));
         assertThat(expression.validate(context)).isEqualTo(expected);
     }
